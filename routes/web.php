ae23bb522e26e61eac2710a54bd70a7e5a4a1249
fajarsid <fajarsuganda79@gmail.com>
2025-08-26 11:30:23 +0700

    add login page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+
+Route::post('/login', [LoginController::class, 'handleLogin'])->name('login');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
